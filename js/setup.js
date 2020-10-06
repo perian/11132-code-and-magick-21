be60733508b69a2fbfcaeabe912f40a24ec2d26b
perian <dmperian@gmail.com>
2020-10-06 21:30:12 +0300

    Внес правки согласно замечания наставника

--- js/setup.js
+++ js/setup.js
@@ -1,46 +1,42 @@
 'use strict';
 
-const names = [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const surnames = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const coatColors = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-const eyeColors = [`black`, `red`, `blue`, `yellow`, `green`];
-let wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
-let wizardList = document.querySelector(`.setup-similar-list`);
+const NAMES = [`Иван`, `Хуан`, `Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+const EYE_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const WIZARDS_AMMOUNT = 4;
+const wizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
+const wizardList = document.querySelector(`.setup-similar-list`);
 
 let setup = document.querySelector(`.setup`);
 setup.classList.remove(`hidden`);
 
-let getRandomFeature = (array) => {
-  let randomInt = Math.floor(Math.random() * array.length);
-  return array[randomInt];
+const getRandomInt = (maxInt) => {
+  let randomInt = Math.floor(Math.random() * maxInt);
+  return randomInt;
 };
 
-let wizards = [
-  {
-    name: getRandomFeature(names) + ` ` + getRandomFeature(surnames),
-    coatColor: getRandomFeature(coatColors),
-    eyeColor: getRandomFeature(eyeColors)
-  },
-  {
-    name: getRandomFeature(names) + ` ` + getRandomFeature(surnames),
-    coatColor: getRandomFeature(coatColors),
-    eyeColor: getRandomFeature(eyeColors)
-  },
-  {
-    name: getRandomFeature(names) + ` ` + getRandomFeature(surnames),
-    coatColor: getRandomFeature(coatColors),
-    eyeColor: getRandomFeature(eyeColors)
-  },
-  {
-    name: getRandomFeature(names) + ` ` + getRandomFeature(surnames),
-    coatColor: getRandomFeature(coatColors),
-    eyeColor: getRandomFeature(eyeColors)
+const getRandomArrayElement = (array) => {
+  return array[getRandomInt(array.length)];
+};
+
+const createFeaturesList = () => {
+  const wizardsFeatureArray = [];
+  for (let i = 0; i < WIZARDS_AMMOUNT; i++) {
+    let wizardFeatures = {
+      name: getRandomArrayElement(NAMES) + ` ` + getRandomArrayElement(SURNAMES),
+      coatColor: getRandomArrayElement(COAT_COLORS),
+      eyeColor: getRandomArrayElement(EYE_COLORS)
+    };
+    wizardsFeatureArray.push(wizardFeatures);
   }
-];
+  return wizardsFeatureArray;
+};
 
+const wizardsFeatures = [] = createFeaturesList();
 
-let createWizard = (wizard) => {
-  let wizardElement = wizardTemplate.cloneNode(true);
+const createWizard = (wizard) => {
+  const wizardElement = wizardTemplate.cloneNode(true);
 
   wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
   wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
@@ -49,10 +45,10 @@ let createWizard = (wizard) => {
   return wizardElement;
 };
 
-let fragment = document.createDocumentFragment();
+const fragment = document.createDocumentFragment();
 
-for (let i = 0; i < wizards.length; i++) {
-  fragment.appendChild(createWizard(wizards[i]));
+for (let i = 0; i < wizardsFeatures.length; i++) {
+  fragment.appendChild(createWizard(wizardsFeatures[i]));
 }
 
 wizardList.appendChild(fragment);
